15571963d8c0b5961fb0d41a9ddc824bb9a9f0fd
CodeOfSomnath <somnathnandi368@gmail.com>
2024-08-25 19:35:27 +0530

    Longest common prefix, I will do it later

---
+++ longest_common_prefix.c
@@ -0,0 +1,37 @@
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+
+*/
+
+// :TODO: it is panding, I can not imagine how to compare properly. I will do it later.
+char* longestCommonPrefix(char** strs, int strsSize) {
+    
+}
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
